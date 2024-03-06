1d9d0b83968075ac095737735289832975149520
anas mohamed <anas2003610@gmail.com>
2024-03-06 17:03:01 +0200

    create edit note view

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,59 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:mynoteapp/view/edit_note_view.dart';
 
 class NotesItem extends StatelessWidget {
   const NotesItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 22, left: 10, right: 2),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'flutter tips',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 22,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(
-                top: 16,
-                bottom: 16,
-              ),
-              child: Text(
-                'Bulid your career whith anas mohamed',
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 22, left: 10, right: 2),
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'flutter tips',
                 style: TextStyle(
-                  color: Colors.black.withOpacity(0.5),
-                  fontSize: 17,
+                  color: Colors.black,
+                  fontSize: 22,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(
+                  top: 16,
+                  bottom: 16,
+                ),
+                child: Text(
+                  'Bulid your career whith anas mohamed',
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(0.5),
+                    fontSize: 17,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.delete,
+                  color: Colors.black,
+                  size: 26,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.delete,
-                color: Colors.black,
-                size: 26,
+            Padding(
+              padding: const EdgeInsets.only(right: 18),
+              child: Text(
+                'march5,2024',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.4),
+                ),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 18),
-            child: Text(
-              'march5,2024',
-              style: TextStyle(
-                color: Colors.black.withOpacity(0.4),
-              ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
